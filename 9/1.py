193967a58dd7b12e7e9936ee2462244807503186
rezatn0934 <rezatn0934@gmail.com>
2023-06-02 14:58:07 +0330

    use re.findall

--- 9/1.py
+++ 9/1.py
@@ -2,6 +2,9 @@ import re
 
 def validate_passw(passw):
     passw = str(passw)
-    if not re.search("[a-z]|[A-Z]|[0-9]{8,}", passw):
-        raise ValueError("pWrong password")
+    if not re.findall(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$", passw):
+        return "Wrong password"
+    else:
+        return "Password is valid"
 
+print(validate_passw("rezatnbjhA2"))
